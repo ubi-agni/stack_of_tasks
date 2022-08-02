770f0a15402ec44d799b836eab51f3a496206df3
Felix Kolesch <fkolesch@uni-bielefeld.de>
2022-08-02 16:02:23 +0200

    Move and update matrix prettyprint

--- python/stack_of_tasks/solver/solver.py
+++ python/stack_of_tasks/solver/solver.py
@@ -4,45 +4,28 @@ import numpy as np
 import osqp
 from scipy import sparse
 
-
-def prettyprintMatrix(inmatrix):
-    matrix = []
-    maxL = 0
-    for row in inmatrix:
-        col = []
-        for x in row:
-            s = f"{x:.2e}"
-            maxL = max(len(s), maxL)
-            col.append(s)
-        matrix.append(col)
-
-    s = ""
-    for r in matrix:
-
-        for x in r:
-            s += f" {x :>^{maxL}} "
-        s += "\n"
-    print(s)
+# from stack_of_tasks.utils import prettyprintMatrix
 
 
 class HQPwithSlacks:
     """The hierarchical solver iteratively solves the tasks at the various levels.
-       At a given level, the task is specified by lb - slack ≤ J dq ≤ ub + slack.
-       Additionally, we need to fulfill the constraints of the previous tasks: J' dq = slack'.
+    At a given level, the task is specified by lb - slack ≤ J dq ≤ ub + slack.
+    Additionally, we need to fulfill the constraints of the previous tasks: J' dq = slack'.
 
-       Thus, we can use fixed size constraint and optimization matrices A and H, which are incrementally filled.
-       The maximal number of slack variables for optimization is the number of soft DoFs of a task at a given level.
-       The constraint matrix has the following shape:
-                            ⎧1
-                         dq ⎨ ⋱
-                            ⎩   1
-                            ⎧     1
-        max slack variables ⎨      ⋱
-                            ⎩        0
-        higher-level tasks  {   J'   0
-        cur. task ub        {   J   -1
-        cur. task lb        {   J   +1
+    Thus, we can use fixed size constraint and optimization matrices A and H, which are incrementally filled.
+    The maximal number of slack variables for optimization is the number of soft DoFs of a task at a given level.
+    The constraint matrix has the following shape:
+                         ⎧1
+                      dq ⎨ ⋱
+                         ⎩   1
+                         ⎧     1
+     max slack variables ⎨      ⋱
+                         ⎩        0
+     higher-level tasks  {   J'   0
+     cur. task ub        {   J   -1
+     cur. task lb        {   J   +1
     """
+
     @staticmethod
     def get_matrix_specs(task_stack):
         max_rows = 0
@@ -74,16 +57,18 @@ class HQPwithSlacks:
 
         self.q = np.zeros(self.max_vars)  # (linear) objective vector
 
-        self.A = np.zeros((self.max_vars + max_rows, self.max_vars))  # constraint matrix for all tasks
+        self.A = np.zeros(
+            (self.max_vars + max_rows, self.max_vars)
+        )  # constraint matrix for all tasks
         self.lb = np.zeros(self.max_vars + max_rows)  # lower bound vector for all tasks
         self.ub = np.zeros(self.max_vars + max_rows)  # upper         "
 
-        self.A[:self.N, :self.N] = np.identity(self.N)  # joint constraint matrix is identity
-        self.lb[:self.N] = lower  # lower joint bounds
-        self.lb[self.N:self.max_vars] = 0  # lower slack bounds
+        self.A[: self.N, : self.N] = np.identity(self.N)  # joint constraint matrix is identity
+        self.lb[: self.N] = lower  # lower joint bounds
+        self.lb[self.N : self.max_vars] = 0  # lower slack bounds
 
-        self.ub[:self.N] = upper  # upper joint bounds
-        self.ub[self.N:self.max_vars] = np.PINF  # upper slack bounds
+        self.ub[: self.N] = upper  # upper joint bounds
+        self.ub[self.N : self.max_vars] = np.PINF  # upper slack bounds
 
     def __iter__(self):  # initialize iterator
         self._index = -1
@@ -100,10 +85,10 @@ class HQPwithSlacks:
         used_rows = self.start_row
         for task in self.tasks[self._index]:
             rows = task.size
-            self.A[used_rows: used_rows + rows, :self.N] = task.A
-            self.A[used_rows: used_rows + rows, used_vars: used_vars + rows] = -np.eye(rows)
-            self.lb[used_rows: used_rows + rows] = np.NINF
-            self.ub[used_rows: used_rows + rows] = task.upper
+            self.A[used_rows : used_rows + rows, : self.N] = task.A
+            self.A[used_rows : used_rows + rows, used_vars : used_vars + rows] = -np.eye(rows)
+            self.lb[used_rows : used_rows + rows] = np.NINF
+            self.ub[used_rows : used_rows + rows] = task.upper
 
             used_vars += rows
             used_rows += rows
@@ -112,17 +97,19 @@ class HQPwithSlacks:
         used_vars = self.N
         for task in self.tasks[self._index]:
             rows = task.size
-            self.A[used_rows: used_rows + rows, :self.N] = task.A
-            self.A[used_rows: used_rows + rows, used_vars: used_vars + rows] = np.eye(rows)
-            self.lb[used_rows: used_rows + rows] = task.lower
-            self.ub[used_rows: used_rows + rows] = np.PINF
+            self.A[used_rows : used_rows + rows, : self.N] = task.A
+            self.A[used_rows : used_rows + rows, used_vars : used_vars + rows] = np.eye(rows)
+            self.lb[used_rows : used_rows + rows] = task.lower
+            self.ub[used_rows : used_rows + rows] = np.PINF
 
             used_vars += rows
             used_rows += rows
 
-        self.A[self.N:used_vars, self.N:used_vars] = np.eye(used_vars - self.N)  # slack constraints
+        self.A[self.N : used_vars, self.N : used_vars] = np.eye(
+            used_vars - self.N
+        )  # slack constraints
         unused = self.max_vars - used_vars  # unused slack variables
-        self.A[used_vars:self.max_vars, used_vars:self.max_vars] = np.zeros((unused, unused))
+        self.A[used_vars : self.max_vars, used_vars : self.max_vars] = np.zeros((unused, unused))
 
         return (
             self.H[:used_vars, :used_vars],
@@ -139,16 +126,20 @@ class HQPwithSlacks:
         for task in self.tasks[self._index]:
             rows = task.size
             fixed = task.A.dot(dq)
-            assert(np.allclose(self.A[self.start_row: self.start_row + rows, :self.N], task.A))
+            assert np.allclose(self.A[self.start_row : self.start_row + rows, : self.N], task.A)
 
-            self.A[self.start_row: self.start_row + rows, self.N:] = 0  # clear slacks from constraint rows
-            self.lb[self.start_row: self.start_row + rows] = fixed
-            self.ub[self.start_row: self.start_row + rows] = fixed
+            self.A[
+                self.start_row : self.start_row + rows, self.N :
+            ] = 0  # clear slacks from constraint rows
+            self.lb[self.start_row : self.start_row + rows] = fixed
+            self.ub[self.start_row : self.start_row + rows] = fixed
 
             used_rows += rows
             self.start_row += rows  # increase start position for next iteration
 
-        self.A[self.start_row: self.start_row + used_rows, self.N:] = 0  # clear slacks from all remaining (lb) rows
+        self.A[
+            self.start_row : self.start_row + used_rows, self.N :
+        ] = 0  # clear slacks from all remaining (lb) rows
 
 
 class Solver:
@@ -168,7 +159,7 @@ class Solver:
                 print(sol.info.status)
                 # return None, None      # TODO handling of failed task!
             else:
-                dq, slack = sol.x[:self.N], sol.x[self.N:]
+                dq, slack = sol.x[: self.N], sol.x[self.N :]
                 desc.nullspace_constraint(dq)
                 task_residuals.append(slack)
 
--- python/stack_of_tasks/utils.py
+++ python/stack_of_tasks/utils.py
@@ -2,3 +2,30 @@ class Callback(list):
     def __call__(self, *args) -> None:
         for listener in self:
             listener(*args)
+
+
+def prettyprintMatrix(inmatrix):
+    matrix = []
+    maxL = 0
+    for row in inmatrix:
+        col = []
+        for x in row:
+            s = f"{x:.2e}"
+            maxL = max(len(s), maxL)
+            col.append(s)
+        matrix.append(col)
+
+    s = ""
+    for r in matrix[:-1]:
+        for x in r[:-1]:
+            s += f"{x :^{maxL}}│"
+        s += f"{r[-1] :^{maxL}}"
+        s += "\n"
+        s += ("─" * maxL + "┼") * (len(r) - 1) + "─" * maxL
+        s += "\n"
+
+    for r in matrix[-1][:-1]:
+        s += f"{r :^{maxL}}│"
+    s += f"{matrix[-1][-1] :^{maxL}}"
+
+    return s

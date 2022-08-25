935e46122be244eb02841c5b507ca2ca09b072fc
Felix Kolesch <fkolesch@uni-bielefeld.de>
2022-08-25 15:27:59 +0200

    💥 🎨 Restructure Task classes

---
+++ python/stack_of_tasks/tasks/Ieq_Tasks.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from stack_of_tasks.utils import skew
+
+from .Task import IeqTask
+
+
+class ConeTask(IeqTask):
+    name = "Cone"
+
+    def _compute(self, J, current, target, threshold, target_axis, robot_axis):
+        """Align axis in eef frame to lie in cone spanned by reference axis and opening angle acos(threshold)"""
+
+        threshold = np.cos(threshold)
+        # transform axis from eef frame to base frame
+        axis = current[0:3, 0:3].dot(robot_axis)
+        reference = target[0:3, 0:3].dot(target_axis)
+
+        self.A = np.array([reference.T.dot(skew(axis)).dot(J[3:])])
+        self.upper_bound = np.array([reference.T.dot(axis) - threshold])
+        self.lower_bound = np.array([-10e20])

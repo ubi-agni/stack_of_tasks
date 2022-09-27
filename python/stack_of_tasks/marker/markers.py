22d9858c5bc3fbf3feeaa310010cd86396271eac
Felix Kolesch <fkolesch@uni-bielefeld.de>
2022-09-27 11:27:58 +0200

    🐛 🎨  Transfer cone marker to new marker class

--- python/stack_of_tasks/marker/markers.py
+++ python/stack_of_tasks/marker/markers.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
 from interactive_markers.interactive_marker_server import (
     InteractiveMarkerFeedback,
     InteractiveMarkerServer,
@@ -8,7 +10,7 @@ from tf import transformations as tf
 from visualization_msgs.msg import InteractiveMarkerControl
 
 from .interactive_marker import IAMarker
-from .utils import pose_to_matrix
+from .utils import create_pose, pose_to_matrix
 
 
 class ControlMarker(IAMarker, ABC):
@@ -86,3 +88,96 @@ class SixDOFMarker(ControlMarker):
         )
         self._add_movement_control(self.marker, "", InteractiveMarkerControl.MOVE_AXIS)
         self._add_movement_control(self.marker, "", InteractiveMarkerControl.ROTATE_AXIS)
+
+
+class ConeMarker(IAMarker):
+    def __init__(
+        self,
+        server: InteractiveMarkerServer = None,
+        pose=tf.translation_matrix([0, 0, 0]),
+        name: str = "Cone",
+        scale: float = 1,
+        angle=0.4,
+        mode=InteractiveMarkerControl.ROTATE_3D,
+    ) -> None:
+        super().__init__(
+            server=server, name=name, pose=pose, scale=scale, angle=angle, mode=mode
+        )
+
+        self._angle: int
+        self._scale: int
+
+    def _setup_marker(self, name, pose, scale, angle, mode):
+        self._angle = angle
+        self._scale = scale
+
+        loc_marker = self._create_interactive_marker(
+            self.server,
+            f"{self.name}_Pos",
+            pose=pose,
+            scale=0.1,
+            callback=self._callback_pose,
+        )
+
+        self._add_display_marker(loc_marker, "", IAMarker.cone(self._angle, self._scale))
+        self._add_movement_control(loc_marker, "", InteractiveMarkerControl.MOVE_AXIS)
+        self._add_movement_control(loc_marker, "", InteractiveMarkerControl.ROTATE_AXIS)
+
+        handle_marker = self._create_interactive_marker(
+            self.server,
+            f"{self.name}_Handle",
+            scale=0.05,
+            callback=self._callback_angel,
+            pose=tf.translation_matrix([0, 0, 0]),
+        )
+        self._add_movement_marker(
+            handle_marker, "", InteractiveMarkerControl.MOVE_PLANE, marker=self.sphere()
+        )
+        self._add_movement_control(
+            handle_marker, "", InteractiveMarkerControl.MOVE_AXIS, directoins="z"
+        )
+        self._add_movement_control(
+            handle_marker, "", InteractiveMarkerControl.MOVE_AXIS, directoins="y"
+        )
+        self.server.applyChanges()
+        self._calc_handle_pose(pose)
+
+        self._data_callback(f"{self.name}_pose", pose)
+        self._data_callback(f"{self.name}_angle", self._angle)
+
+    def provided_targets(self):
+        return ["{self.name}_Pos", "{self.name}_Handle"]
+
+    def delete(self):
+        self.server.erase(f"{self.name}_Pos")
+        self.server.erase(f"{self.name}_Handle")
+        self.server.applyChanges()
+
+    def _calc_handle_pose(self, T_root):
+        handle_pose = tf.rotation_matrix(self._angle, [1, 0, 0]).dot(
+            tf.translation_matrix([0, 0, self._scale])
+        )
+        self.server.setPose(f"{self.name}_Handle", create_pose(T_root.dot(handle_pose)))
+        self.server.applyChanges()
+
+    def _callback_pose(self, feedback):
+        T = pose_to_matrix(feedback.pose)
+        self._calc_handle_pose(T)
+        self._data_callback(f"{self.name}_pose", T)
+        self.server.applyChanges()
+
+    def _callback_angel(self, feedback):
+        T_marker = pose_to_matrix(feedback.pose)
+        coneM = self._get_marker(f"{self.name}_Pos")
+        T_cone = pose_to_matrix(coneM.pose)
+
+        # vector from cone's origin to marker
+        v = T_marker[0:3, 3] - T_cone[0:3, 3]
+        self._scale = np.linalg.norm(v)
+        self._angle = np.arccos(np.maximum(0, T_cone[0:3, 2].dot(v) / self._scale))
+
+        self._calc_handle_pose(T_cone)
+        coneM.controls[0].markers[0].points = IAMarker.cone(self._angle, self._scale).points
+        self._update_marker(coneM)
+        self._data_callback(f"{self.name}_angle", self._angle)
+        self.server.applyChanges()

709734d304774f87336705dc9caacd377d838508
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-02-14 11:14:36 +0100

    Fix RefFramesModel + RefComboBox
    We need to implement data(..., Qt.EditRole) as this is used to set the combo's lineedit.

--- python/stack_of_tasks/ui/widgets/ref_combobox.py
+++ python/stack_of_tasks/ui/widgets/ref_combobox.py
@@ -12,29 +12,12 @@ class RefComboBox(QComboBox):
         self.setEditable(editable)
         # TODO: Insertion requires RefFramesModel.insertRows() to be implemented!
         self.setInsertPolicy(QComboBox.NoInsert)
-        if editable:
-            self.currentIndexChanged.connect(self.update_current_text)
         if model is not None:
             self.setModel(model)
-
-    def setCurrentIndex(self, row: int) -> None:
-        super().setCurrentIndex(row)
-        if self.isEditable():  # sync currentText with currentIndex
-            self.update_current_text(row)
-
-    @QtCore.pyqtSlot(int)
-    def update_current_text(self, row: int):
-        index = self.model().index(row, 0, QtCore.QModelIndex())
-        self.setCurrentText(index.data())
 
     def focusInEvent(self, e: QtGui.QFocusEvent) -> None:
         self.update_completer_model()  # update completer model when entering
         super().focusInEvent(e)
-
-    def focusOutEvent(self, e: QtGui.QFocusEvent) -> None:
-        super().focusOutEvent(e)
-        # bugfix: currentText is erased when leaving
-        self.update_current_text(self.currentIndex())
 
     @QtCore.pyqtSlot()
     def update_completer_model(self):

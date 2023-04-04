361a5c4bce36dcf41ca0210f9058cf6f298b2ec6
Felix Kolesch <fkolesch@uni-bielefeld.de>
2023-04-04 20:36:11 +0200

    Fix link name attribute

--- python/stack_of_tasks/ref_frame/frames.py
+++ python/stack_of_tasks/ref_frame/frames.py
@@ -8,6 +8,8 @@ from stack_of_tasks.robot_model.robot_state import RobotState
 
 
 class Origin(RefFrame):
+    """Defines the origin. T is the identity and can not be changed."""
+
     T: Transform = ta.ReadOnly(np.identity(4))
 
     def __init__(self) -> None:
@@ -16,8 +18,17 @@ class Origin(RefFrame):
 
 
 class RobotRefFrame(RefFrame):
+    """A reference to a robot frame. Contains current transform (T) and Jacobian (J) of the given robot link. All attributes are read only and will be calculated dynamically.
+
+    Attributes:
+        robot_state (RobotState): A read only reference to the frames robot state.
+        link_name (str): The frames robot link name. Read only.
+        T (Transform): The current robot link transform.
+        J (Jacobian): The current robot link jacobian.
+    """
+
     robot_state: RobotState = ta.Instance(RobotState)
-    link: str = ta.ReadOnly()
+    link_name: str = ta.ReadOnly()
 
     T = ta.Property(observe="_fk")
     J = ta.Property(observe="_fk")
@@ -25,11 +36,11 @@ class RobotRefFrame(RefFrame):
     _fk = ta.Property(observe="robot_state.joint_values")
 
     def __init__(self, state: RobotState, link: str) -> None:
-        super().__init__(robot_state=state, link=link)
+        super().__init__(robot_state=state, link_name=link)
 
     @ta.cached_property
     def _get__fk(self):
-        return self.robot_state.fk(self.link)
+        return self.robot_state.fk(self.link_name)
 
     def _get_T(self):
         return self._fk[0]

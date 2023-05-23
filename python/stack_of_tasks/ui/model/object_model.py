0c170630e766d54d3bd0d76613cb2d1cdde9b9d2
Felix Kolesch <fkolesch@uni-bielefeld.de>
2023-05-23 16:40:20 +0200

    Improve object model

--- python/stack_of_tasks/ui/model/object_model.py
+++ python/stack_of_tasks/ui/model/object_model.py
@@ -1,32 +1,106 @@
 from __future__ import annotations
 
-from typing import Generic, List, Optional, TypeVar
+from typing import Callable, Generic, List, Optional, Type, TypeVar
 
-from PyQt5.QtCore import QAbstractListModel, QModelIndex, Qt
+from PyQt5.QtCore import QAbstractListModel, QModelIndex, QObject, QSortFilterProxyModel, Qt
 
-from . import RawDataRole
+from stack_of_tasks.ui.model import RawDataRole
 
 ObjectType = TypeVar("ObjectType")
 
 
+def display_cls_name(obj: object) -> str:
+    if isinstance(obj, type):
+        return obj.__name__
+
+    else:
+        return obj.__class__.__name__
+
+
 class ObjectModel(Generic[ObjectType], QAbstractListModel):
-    def __init__(self, parent=None, data: Optional[List[ObjectType]] = None) -> None:
+    def __init__(
+        self,
+        data: Optional[List[ObjectType]] = None,
+        disp_func: Optional[Callable[[ObjectType], str]] = None,
+        parent=None,
+    ) -> None:
         super().__init__(parent)
 
-        self._additional_data = []
         self._objs: List[ObjectType] = [] if data is None else data
+        self._disp_func = disp_func
+
+    def rowOf(self, obj: ObjectType) -> int:
+        return self._objs.index(obj)
 
     def rowCount(self, parent=None) -> int:
         return len(self._objs)
 
+    def set_data_list(self, lst: List):
+        self.beginResetModel()
+        self._objs = lst
+        self.endResetModel()
+
+    def get_display_string(self, index: QModelIndex) -> str:
+        if self._disp_func is not None:
+            return self._disp_func(self._objs[index.row()])
+
+        return str(self._objs[index.row()])
+
     def data(self, index: QModelIndex, role: int = Qt.DisplayRole) -> str | ObjectType | None:
         if role == Qt.DisplayRole:
-            return str(self._additional_data[index.row()]["name"])
+            return self.get_display_string(index)
         elif role == RawDataRole:
             return self._objs[index.row()]
 
-    def add_object(self, obj: ObjectType, **additional_data):
-        self.beginInsertRows(QModelIndex(), len(self._objs), len(self._objs))
-        self._additional_data.append(additional_data)
-        self._objs.append(obj)
+    def item_changed(self, idx: int | slice):
+        if isinstance(idx, int):
+            start = self.index(idx, 0)
+            end = start
+        else:
+            start = self.index(idx.start, 0)
+            end = self.index(idx.stop, 0)
+        self.dataChanged.emit(start, end)
+
+    def items_inserted(self, idx: int | slice):
+        if isinstance(idx, int):
+            start = idx
+            end = start
+        else:
+            start = idx.start
+            end = idx.stop
+
+        self.beginInsertRows(QModelIndex(), start, end)
         self.endInsertRows()
+
+    def items_removed(self, idx: int | slice):
+        if isinstance(idx, int):
+            start = idx
+            end = start
+        else:
+            start = idx.start
+            end = idx.stop
+        self.beginRemoveRows(QModelIndex(), start, end)
+        self.endRemoveRows()
+
+
+class FilterObjectModel(QSortFilterProxyModel):
+    def __init__(
+        self,
+        source: ObjectModel,
+        filter_type: Type,
+        filter_subclass=False,
+        parent: QObject | None = ...,
+    ) -> None:
+        super().__init__(parent)
+        self.setSourceModel(source)
+        self._filter = filter_type
+        self._subclass = filter_subclass
+
+    def setSourceModel(self, sourceModel: ObjectModel) -> None:
+        return super().setSourceModel(sourceModel)
+
+    def filterAcceptsRow(self, source_row: int, source_parent: QModelIndex) -> bool:
+        if self._subclass:
+            return issubclass(source_parent.child(source_row).data(RawDataRole), self._filter)
+        else:
+            return isinstance(source_parent.child(source_row).data(RawDataRole), self._filter)

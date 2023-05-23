4afc0fdf6dfc3ffdc82aa5d3de9fe754348ae755
Felix Kolesch <fkolesch@uni-bielefeld.de>
2023-05-23 17:26:31 +0200

    Remove unused data input module

--- python/stack_of_tasks/ui/hierarchy_tab/task_details.py
+++ python/stack_of_tasks/ui/hierarchy_tab/task_details.py
@@ -1,7 +1,6 @@
 from PyQt5.QtWidgets import QWidget
 
 from stack_of_tasks.tasks.Task import RelativeTask, TaskSoftnessType
-from stack_of_tasks.ui.data_input import DataInput
 from stack_of_tasks.ui.model.enum_model import EnumModel, RawDataRole
 from stack_of_tasks.ui.traits_mapping import HasTraitGroupBox, HasTraitPropertyMixin
 

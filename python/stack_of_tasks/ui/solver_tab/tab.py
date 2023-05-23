2501c19b9d66f0af3e15ce444db54bb7639e52db
Felix Kolesch <fkolesch@uni-bielefeld.de>
2023-05-23 16:36:11 +0200

    Simplify solver-tab

--- python/stack_of_tasks/ui/solver_tab/tab.py
+++ python/stack_of_tasks/ui/solver_tab/tab.py
@@ -1,27 +1,22 @@
-from PyQt5.QtWidgets import QDoubleSpinBox, QFormLayout, QVBoxLayout, QWidget
+from PyQt5 import QtWidgets
 
-from stack_of_tasks.solver import InverseJacobianSolver, OSQPSolver
-from stack_of_tasks.ui.generated.SolverSettings import Ui_SolverSettings
-from stack_of_tasks.ui.model.object_model import ObjectModel, RawDataRole
-from stack_of_tasks.ui.traits_mapping import HasTraitGroupBox
+from stack_of_tasks.ui.traits_mapping import HasTraitPropertyMixin
 from stack_of_tasks.ui.traits_mapping.custom_widgets.object_dropbown import ObjectDropdown
 
 
-class SolverTab(QWidget):
+class SolverTab(QtWidgets.QWidget):
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
 
-        self.vl = QVBoxLayout()
-        self.setLayout(self.vl)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.cls_layout = QtWidgets.QFormLayout()
+        self.cls_layout.setFieldGrowthPolicy(QtWidgets.QFormLayout.FieldsStayAtSizeHint)
 
-        self.solver_class = ObjectDropdown()
-        fl1 = QFormLayout()
-        fl1.addRow("Solver", self.solver_class)
-        self.vl.addLayout(fl1)
+        self.solverClassComboBox = ObjectDropdown()
+        self.cls_layout.addRow("Class", self.solverClassComboBox)
 
-        self.solver_settings = HasTraitGroupBox()
-        self.vl.addWidget(self.solver_settings)
-
-        self.solver_class.currentTextChanged.connect(
-            lambda title: self.solver_settings.setTitle(f"{title} - Settings")
-        )
+        self.verticalLayout.addLayout(self.cls_layout)
+        self.edit_solver = HasTraitPropertyMixin()
+        self.verticalLayout.addWidget(self.edit_solver)
+        self.verticalLayout.setStretch(1, 1)
+        self.setLayout(self.verticalLayout)

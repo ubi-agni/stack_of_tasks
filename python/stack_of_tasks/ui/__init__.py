10219bf6cfd25b3986cb33d4bf3d2cade0fae92d
Felix Kolesch <fkolesch@uni-bielefeld.de>
2023-08-13 23:05:46 +0200

    Add ui constants

---
+++ python/stack_of_tasks/ui/__init__.py
@@ -0,0 +1,9 @@
+from PyQt5.QtCore import Qt
+
+RawDataRole = Qt.UserRole + 1
+MappingEntryRole = Qt.UserRole + 2
+TraitRole = Qt.UserRole + 3
+ItemRole = Qt.UserRole + 3
+
+
+DISPLAY_STRING_ATTR = "display_text"

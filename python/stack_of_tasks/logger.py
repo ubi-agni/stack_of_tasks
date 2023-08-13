831e2ec74201acc1ddb0a48efe67940c248c7f19
Felix Kolesch <fkolesch@uni-bielefeld.de>
2023-08-13 23:06:46 +0200

    Add logging with fix for rospy logs

---
+++ python/stack_of_tasks/logger.py
@@ -0,0 +1,18 @@
+import logging
+from logging import LogRecord
+
+
+class RosFilter(logging.Filter):
+    def filter(self, record: LogRecord) -> bool:
+        print(vars(record))
+        return True
+
+
+def fix_rospy_logging(logger: logging.Logger):
+    console = logging.StreamHandler()
+    formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+    console.setFormatter(formatter)
+    logger.root.addHandler(console)
+
+
+sot_logger = logging.getLogger("sot")

644aaec40299fb6e695ad6f4e3b898a14e876b9c
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-11-08 23:40:52 +0100

    Move joint_state_msg from RobotState to JointStatePublisher

--- python/stack_of_tasks/robot_model/jointstate_publisher.py
+++ python/stack_of_tasks/robot_model/jointstate_publisher.py
@@ -12,14 +12,17 @@ class JointStatePublisher(BaseSoTHasTraits):
     robot_state = ta.Instance(RobotState)
 
     def __init__(self, robot_state: RobotState, ns_prefix: str = "") -> None:
-        self.ns_prefix = ns_prefix
+        super().__init__(robot_state=robot_state)
 
+        self.ns_prefix = ns_prefix
         self._pub = rospy.Publisher(
             ns_prefix + "target_joint_states", JointState, queue_size=1, latch=True
         )
 
-        super().__init__(robot_state=robot_state)
+        self.joint_state_msg = JointState()
+        self.joint_state_msg.name = [j.name for j in robot_state.robot_model.active_joints]
 
     @ta.on_trait_change("robot_state.joint_values")
     def publish_joints(self):
-        self._pub.publish(self.robot_state.joint_state_msg)
+        self.joint_state_msg.position = self.robot_state.joint_values
+        self._pub.publish(self.joint_state_msg)

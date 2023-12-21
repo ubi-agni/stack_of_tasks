0a26c819d5160ffd2f57144ebe92f223273c80e0
Felix Kolesch <fkolesch@uni-bielefeld.de>
2023-12-21 17:55:24 +0100

    Monkeypatch Enum to have a sublcass register

--- python/stack_of_tasks/__init__.py
+++ python/stack_of_tasks/__init__.py
@@ -1,1 +1,20 @@
+import enum
+
+from stack_of_tasks.utils.class_register import ClassRegister
+
+# Monkeypatch Enum to have a sublcass register.
+cl = ClassRegister("Enums")
+
+
+@classmethod
+def __init_subclass__(cls, **kwargs):
+    super.__init_subclass__(**kwargs)
+    for f in cls.__init_subclass_hooks__:
+        f(cls)
+
+
+enum.Enum.__init_subclass__ = __init_subclass__
+enum.Enum = cl.base(enum.Enum)
+
+
 from .controller import Controller

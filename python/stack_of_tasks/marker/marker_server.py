98959d6deafc32f1ff98a419dfedbaaf5ed7c459
Felix Kolesch <fkolesch@uni-bielefeld.de>
2023-12-21 17:44:47 +0100

    Add remove function to marker-server

--- python/stack_of_tasks/marker/marker_server.py
+++ python/stack_of_tasks/marker/marker_server.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from weakref import WeakValueDictionary
+
 from traits.api import Dict, HasTraits, observe
 from traits.observation.events import TraitChangeEvent
 
@@ -12,8 +14,8 @@ from .abstract_marker import IAMarker
 
 
 class MarkerServer(BaseSoTHasTraits):
-    marker = Dict(str, IAMarker)
-    _control = Dict(str, IAMarker)
+    marker: dict = Dict(str, IAMarker)
+    _control: dict = Dict(str, IAMarker)
 
     def __init__(self, namespace="controller") -> None:
         super().__init__()
@@ -27,14 +29,20 @@ class MarkerServer(BaseSoTHasTraits):
             self._ims.insert(m, self._server_cb)
         self._ims.applyChanges()
 
+    def remove(self, marker: IAMarker):
+        for control in marker.markers:
+            self._ims.erase(control.name)
+            self._control.pop(control.name)
+
+        self._ims.applyChanges()
+        self.marker.pop(marker.name)
+
     @observe("marker.items:sync")
     def _sync(self, evt: TraitChangeEvent):
-        # self._ims.setPose()
         for marker in self.marker[evt.new].markers:
             self._ims.setPose(marker.name, marker.pose)
             self._ims.insert(marker, self._server_cb)
         self._ims.applyChanges()
-        # print(evt)
 
     def _server_cb(self, fb: InteractiveMarkerFeedback):
         self._control[fb.marker_name].feedback(fb)

6ee1695f16434583e761d0b00680961537dbec92
Felix Kolesch <fkolesch@uni-bielefeld.de>
2024-01-08 18:44:31 +0100

    Use trait information in dependency injection

--- python/stack_of_tasks/ui/utils/dependency_injection.py
+++ python/stack_of_tasks/ui/utils/dependency_injection.py
@@ -4,6 +4,8 @@ from stack_of_tasks.ui.model_mapping import InjectionArg
 
 T = TypeVar("T")
 
+import traits.api as ta
+
 
 class DependencyInjection:
     mapping = {}
@@ -21,6 +23,19 @@ class DependencyInjection:
         return injected
 
     @staticmethod
+    def _is_injected(val):
+        return isinstance(val, str)
+
+    @staticmethod
     def create_instance(instance_cls: Type[T], kwargs: dict) -> T:
+        if issubclass(instance_cls, ta.HasTraits):
+            inj_t = instance_cls.class_traits(injected=DependencyInjection._is_injected)
+
+            for k in inj_t.values():
+                param_name = k.injected
+                if param_name in DependencyInjection.mapping:
+                    kwargs[param_name] = DependencyInjection.mapping[param_name]
+
         kwargs.update(DependencyInjection.inject(kwargs))
+        print(kwargs)
         return instance_cls(**kwargs)

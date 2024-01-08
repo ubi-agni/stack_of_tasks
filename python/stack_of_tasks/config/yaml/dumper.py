86f7dd4635102e6bf5003ca72bde9af82c71aed0
Felix Kolesch <fkolesch@uni-bielefeld.de>
2024-01-08 18:10:29 +0100

    Add multiple type dumper and loader

--- python/stack_of_tasks/config/yaml/dumper.py
+++ python/stack_of_tasks/config/yaml/dumper.py
@@ -6,7 +6,10 @@ from enum import Enum
 from typing import Any
 
 import yaml
+from numpy import ndarray
+from scipy.spatial.transform.rotation import Rotation
 
+from stack_of_tasks.marker import IAMarker
 from stack_of_tasks.ref_frame import RefFrame
 from stack_of_tasks.utils.traits import BaseSoTHasTraits
 
@@ -46,15 +49,17 @@ class SotDumper(yaml.Dumper):
             sort_keys,
         )
 
-        self.categories = {RefFrame: "frames"}
+        self.categories = {IAMarker: "marker", RefFrame: "frames"}
 
         self.add_multi_representer(BaseSoTHasTraits, SotDumper.task_representer)
         self.add_multi_representer(Enum, SotDumper.enum_representer)
+        self.add_representer(ndarray, SotDumper.array_representer)
 
     def represent(self, data):
-        x = self.represent_data(data)
+        _ = self.represent_data(data)
 
         full_data = {"sot": data}
+
         for obj in self.object_keeper:
             for cls, category in self.categories.items():
                 if isinstance(obj, cls):
@@ -70,14 +75,24 @@ class SotDumper(yaml.Dumper):
 
     @staticmethod
     def task_representer(dumper, data: BaseSoTHasTraits):
-        state = data.__getstate__()
-        state.pop("__traits_version__")
+        state = data.__get_yml_state__()
 
         return dumper.represent_mapping(f"!SOT_{data.__class__.__name__}", state)
 
     @staticmethod
     def enum_representer(dumper, data: Enum):
         return dumper.represent_scalar("!enum", f"{type(data).__name__}.{data.name}")
+
+    def array_representer(self, data: ndarray):
+        if len((s := data.shape)) == 2 and s[0] == 4 and s[1] == 4:
+            xyz = data[:3, 3].tolist()
+            rpy = Rotation.from_matrix(data[:3, :3]).as_euler("ZYX").tolist()
+
+            return self.represent_mapping("transform", {"xyz": xyz, "rpy": rpy}, True)
+        else:
+            ln = self.represent_sequence("!arr", data.tolist(), True)
+            ln.flow_style = True
+            return ln
 
 
 def dump(data):
--- python/stack_of_tasks/config/yaml/loader.py
+++ python/stack_of_tasks/config/yaml/loader.py
@@ -5,17 +5,23 @@ from enum import Enum
 
 from typing import Any
 
+import numpy as np
 import yaml
+from scipy.spatial.transform.rotation import Rotation
 
+from stack_of_tasks.ui.utils.dependency_injection import DependencyInjection
 from stack_of_tasks.utils.traits import BaseSoTHasTraits, register_all
 
 
 class SotYamlLoader(yaml.Loader):
-    def __init__(self, stream: _ReadStream) -> None:
+    def __init__(self, stream) -> None:
         super().__init__(stream)
 
         self.add_multi_constructor("!SOT_", SotYamlLoader.sot_constructor)
         self.add_constructor("!enum", SotYamlLoader.enum_constructor)
+
+        self.add_constructor("!arr", SotYamlLoader.array_constructor)
+        self.add_constructor("transform", SotYamlLoader.transform_constructor)
 
     @staticmethod
     def sot_constructor(loader: SotYamlLoader, tag_suffix: str, node: yaml.nodes.MappingNode):
@@ -23,21 +29,37 @@ class SotYamlLoader(yaml.Loader):
         if cls is None:
             return "CANNOT FIND CLS"
         else:
-            inst: BaseSoTHasTraits = cls.__new__(cls)
-            print(inst)
             data = loader.construct_mapping(node)
-            inst.__setstate__(data)
+
+            inst: BaseSoTHasTraits = DependencyInjection.create_instance(cls, data)
             return inst
 
     @staticmethod
     def find_sot_cls(cls_name: str):
-        print(register_all.class_names)
         return register_all.find_class_by_name(cls_name)
 
-    @staticmethod
-    def enum_constructor(loader: yaml.Loader, node: yaml.nodes.ScalarNode):
+    def enum_constructor(self, node: yaml.nodes.ScalarNode):
         enum_class, value = node.value.split(".")
-        return (enum_class, value)
+        cls = Enum.register.find_class_by_name(enum_class)
+
+        return cls[value]
+
+    def transform_constructor(self, node: yaml.nodes.MappingNode):
+        t = self.construct_mapping(node, True)
+
+        T = np.identity(4)
+        T[:3, 3] = t["xyz"]
+
+        T[:3, :3] = Rotation.from_euler("ZYX", t["rpy"]).as_matrix()
+        return T
+
+    def array_constructor(self, node: yaml.nodes.SequenceNode):
+        s = self.construct_sequence(node, True)
+        dtype = s
+        while isinstance(dtype, list):
+            dtype = s[0]
+
+        return np.fromiter(s, dtype=type(dtype))
 
 
 def load(toml: str):

cb5516c0384a1d3901a6099c1abf88eefbee1243
Felix Kolesch <fkolesch@uni-bielefeld.de>
2024-10-01 16:01:01 +0200

    Update package files

--- setup.py
+++ setup.py
@@ -4,27 +4,8 @@ from setuptools import setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=[
-        "stack_of_tasks",
-        "stack_of_tasks.robot_model",
-        "stack_of_tasks.ui.utils",
-        "stack_of_tasks.ui.tabs",
-        "stack_of_tasks.ui.widgets",
-        "stack_of_tasks.ui.traits_mapping.editor_entry",
-        "stack_of_tasks.ui.traits_mapping",
-        "stack_of_tasks.ui.traits_mapping.painter_entry",
-        "stack_of_tasks.ui.model",
-        "stack_of_tasks.ui",
-        "stack_of_tasks.ui.property_tree",
-        "stack_of_tasks.ui.generated",
-        "stack_of_tasks.utils",
-        "stack_of_tasks.tasks",
-        "stack_of_tasks.ref_frame",
-        "stack_of_tasks.parameter",
-        "stack_of_tasks.solver",
-        "stack_of_tasks.plot",
-        "stack_of_tasks.marker",
-    ],
+    packages=["stack_of_tasks"],
     package_dir={"": "python"},
 )
+
 setup(**setup_args)
